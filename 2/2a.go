31ce054b64f04c53d47df514b947aff833f60514
Bill Napier <napier@google.com>
2021-12-11 00:01:25 -0500

    Part 2 of day 2.

---
+++ 2/2a.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	f, err := os.Open("data")
+	if err != nil {
+		return
+	}
+
+	depth := 0
+	horiz := 0
+	aim := 0
+	scanner := bufio.NewScanner(f)
+	for scanner.Scan() {
+		line := scanner.Text()
+		parts := strings.Split(line, " ")
+		num, err := strconv.Atoi(parts[1])
+		if err != nil {
+			continue
+		}
+		word := parts[0]
+
+		// Actual logic
+		if word == "forward" {
+			horiz += num
+			depth += aim * num
+		} else if word == "down" {
+			aim += num
+		} else if word == "up" {
+			aim -= num
+		}
+	}
+	fmt.Println(depth * horiz)
+}
